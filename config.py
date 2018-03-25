f036e699e67560df6a7677e720dbe38e87bafd3c
Shuchi Tyagi <shuchit91@gmail.com>
2018-03-25 13:50:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 neoscan = {'url': 'https://neoscan-testnet.io/api/test_net/v1/get_highest_block'}
-rpc = {'url':'http://54.148.128.241:20332'}
+rpc = {'url':'http://34.216.181.81:10332'}
 admin = {'email':'me@shuchityagi.com'}
